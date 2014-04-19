dd2938d48dc37b2e00982e96f949e2a14cf40b61
Robert Sim <robert.sim.2010@sis.smu.edu.sg>
2014-04-20 07:03:13 +0800

    updated default routes to use controller instead

--- hackdo/app/routes.php
+++ hackdo/app/routes.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', 'HomeController@showWelcome');
 Route::resource('members', 'MembersController');
 Route::resource('subscriptions', 'SubscriptionsController');
 Route::resource('contracts', 'ContractsController');
