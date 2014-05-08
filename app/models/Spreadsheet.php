40e0dc51f994f8170f711d9fdc6418ce6b50e8d0
CJ <lim@chernjie.com>
2014-05-09 02:52:35 +0800

    Added support for readCSV, verified fields, improved tests;

--- app/models/Spreadsheet.php
+++ app/models/Spreadsheet.php
@@ -4,21 +4,37 @@ class Spreadsheet
 {
 
     private $configFile = 'config/spreadsheets.json';
-    private $csvFileFormat = "app/database/%s.csv";
+    private $csvFileFormat = 'app/database/%s.csv';
 
     private $name = '';
     private $csvFile = '';
+    private $config = '';
 
     public function __construct($name)
     {
         $this->name = $name;
         $this->csvFile = sprintf($this->csvFileFormat, $this->name);
-        
+        $this->config = json_decode(file_get_contents($this->configFile), true)[$this->name];
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    public function check()
+    {
+        // download file older than one day
+        return file_exists($this->csvFile)
+            && filemtime($this->csvFile) > time() - 24 * 60 * 60
+            ? strlen(file_get_contents($this->csvFile))
+            : $this->downloadCSV();
     }
 
     public function readCSV()
     {
-        if (($handle = fopen($this->configFile, "r")) !== FALSE) {
+        $csv = array();
+        if (($handle = fopen($this->csvFile, "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1024, ",")) !== FALSE) {
                 if (empty($header)) {
                     $header = $data;
@@ -34,13 +50,12 @@ class Spreadsheet
 
     public function downloadCSV()
     {
-        $config = json_decode(file_get_contents($this->configFile), true)[$this->name];
         $spreadsheetUrl = "https://docs.google.com/spreadsheet/pub?"
             . http_build_query(array(
                   'single' => 'true'
                 , 'output' => 'csv'
-                , 'gid' => $config['gid']
-                , 'key' => $config['key']
+                , 'gid' => $this->config['gid']
+                , 'key' => $this->config['key']
         ));
         
         $fileSize = file_put_contents($this->csvFile, file_get_contents($spreadsheetUrl));
--- app/tests/SpreadsheetTest.php
+++ app/tests/SpreadsheetTest.php
@@ -3,21 +3,33 @@
 class SpreadsheetTest extends TestCase
 {
 
-    public function testDownloadMembersCsv()
+    public function testMembersCsv()
     {
         $csv = new Spreadsheet('members');
-        $this->assertGreaterThan(0, $csv->downloadCSV());
+        $this->assertGreaterThan(0, $csv->check());
+        $data = $csv->readCSV();
+        $this->assertInternalType('array', $data);
+        foreach ($csv->getConfig()['fields'] as $field)
+            $this->assertArrayHasKey($field, reset($data));
     }
 
-    public function testDownloadPaypalCsv()
+    public function testPaypalCsv()
     {
         $csv = new Spreadsheet('paypal');
-        $this->assertGreaterThan(0, $csv->downloadCSV());
+        $this->assertGreaterThan(0, $csv->check());
+        $data = $csv->readCSV();
+        $this->assertInternalType('array', $data);
+        foreach ($csv->getConfig()['fields'] as $field)
+            $this->assertArrayHasKey($field, reset($data));
     }
 
-    public function testDownloadStanchartCsv()
+    public function testStanchartCsv()
     {
         $csv = new Spreadsheet('stanchart');
-        $this->assertGreaterThan(0, $csv->downloadCSV());
+        $this->assertGreaterThan(0, $csv->check());
+        $data = $csv->readCSV();
+        $this->assertInternalType('array', $data);
+        foreach ($csv->getConfig()['fields'] as $field)
+            $this->assertArrayHasKey($field, reset($data));
     }
 }
